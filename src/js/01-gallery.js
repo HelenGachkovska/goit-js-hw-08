2c9481940d7947873c76b8c09c701e2a90d57e6f
HelenGachkovska <ogachkovska@gmail.com>
2023-03-14 17:50:15 +0200

    1 & 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const divEl = document.querySelector(".gallery");
+
+const galleryEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+  })
+  .join("");
+
+divEl.innerHTML = galleryEl;
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captions:true, captionsData: "alt", captionDelay: 250});
+
+
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const formData = {};
+
+formEl.addEventListener('input', throttle(function (ev) {
+    const { name, value } = ev.target;
+    formData[name] = value;
+    console.log(formData);
+}, 500));
+
 
