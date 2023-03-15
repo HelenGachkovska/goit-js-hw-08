83daa2ef3468d871868474467449d77f6260d069
HelenGachkovska <ogachkovska@gmail.com>
2023-03-15 11:07:12 +0200

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,10 +3,33 @@ import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
 const formData = {};
 
-formEl.addEventListener('input', throttle(function (ev) {
+formEl.addEventListener(
+  'input',
+  throttle(function (ev) {
     const { name, value } = ev.target;
     formData[name] = value;
-    console.log(formData);
-}, 500));
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }, 500)
+);
 
+formEl.addEventListener('submit', ev => {
+  ev.preventDefault();
+  ev.currentTarget.reset();
 
+  console.log(formData);
+
+  localStorage.clear();
+});
+
+window.addEventListener('load', ev => {
+  const saveData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const inputEl = document.querySelector('input');
+  const messageEl = document.querySelector('textarea');
+
+  if (saveData === null) {
+    return;
+  } else {
+    inputEl.value = saveData.email;
+    messageEl.value = saveData.message;
+  }
+});
